f44ac9ff7e651108d2d23620bea7da325d74bb5a
pozolin <yystudent6@gmail.com>
2025-09-26 19:47:41 -0400

    Added PrintInfo.java to handle printing of book info

---
+++ src/PrintInfo.java
@@ -0,0 +1,8 @@
+public class PrintInfo {
+    public void printBookDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("Price: $" + price);
+    }
+}
