58bc4b1ef893c54125215fbb5206e83947da77a1
Samuel Apoya <sapoya26@colby.edu>
2025-09-27 11:36:48 -0400

    Encapsulated cart mutation with removeByProductName to avoid external list edits and potential ConcurrentModification

--- src/Cart.java
+++ src/Cart.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class Cart {
     private ArrayList<CartItem> items;
-    
+
     public Cart() {
         items = new ArrayList<>();
     }
-    
+
     public void addItem(CartItem item) {
         items.add(item);
     }
@@ -14,7 +14,7 @@ public class Cart {
     public void removeItem(CartItem item) {
         items.remove(item);
     }
-    
+
     public void updateQuantity(CartItem item, int quantity) {
         for (CartItem cartItem : items) {
             if (cartItem.equals(item)) {
@@ -23,7 +23,17 @@ public class Cart {
             }
         }
     }
-    
+
+    /** Encapsulated remover to avoid mutating the list via getItems() while iterating */
+    public void removeByProductName(String name) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equals(name)) {
+                items.remove(i);
+                return;
+            }
+        }
+    }
+
     public void viewCartDetails() {
         System.out.println("Cart Details:");
         for (CartItem item : items) {
@@ -31,7 +41,7 @@ public class Cart {
         }
         System.out.println("\n");
     }
-    
+
     public ArrayList<CartItem> getItems() {
         return items;
     }
