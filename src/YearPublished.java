907830d092b2217db37249269df98a8cae0b9f4f
featgunna <tim@Timurs-MacBook-Pro-161.local>
2025-09-28 23:54:38 -0400

    Created a separate method for year validation

--- src/YearPublished.java
+++ src/YearPublished.java
@@ -1,11 +1,19 @@
 public class YearPublished {
     private final int year;
+    private final int currentYear = 2025;
 
     public YearPublished(int year) {
-        if (year <= 0 && year >= 2026) {
+        if (!validateYear(year)) {
             throw new IllegalArgumentException("Year must be valid");
         }
         this.year = year;
+    }
+
+    private boolean validateYear(int year) {
+        if (year <= 0 || year >= (currentYear + 1)) {
+            return false;
+        }
+        return true;
     }
 
     public int getValue() {
